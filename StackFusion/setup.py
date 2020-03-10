2136e8031b8c6fe2396b310b7661d50eb9153184
yashasvi-goel <yashasvigoel@gmail.com>
2020-03-10 14:20:54 +0530

    setup.py

---
+++ StackFusion/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='StackFusionBackEnd',
+    version='1.0.0',
+    packages=['forms', 'forms.migrations', 'StackFusion'],
+    url='',
+    license='',
+    author='yashasvi goel',
+    author_email='yashasvigoel@gmail.com',
+    description=''
+)
